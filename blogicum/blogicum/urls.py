59b72d5fb741d477264afc659a518b227f3c2ffb
haku4130 <andrejosipov19120@gmail.com>
2023-08-07 11:07:20 +0300

    Рабочая версия сайта

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
-    path('post/', include('pages.urls', namespace='pages')),
+    path('pages/', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
 ]
--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail')
+    path('about/', views.about, name='about'),
+    path('rules/', views.rules, name='rules'),
 ]
